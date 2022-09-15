fb5cd555f088408bfa996016490e0809cda207b7
njnj <njnj@phost.nj>
2022-09-15 21:05:16 +0430

    add block struct and transections into projects

---
+++ njBlockChain/Block.cs
@@ -0,0 +1,28 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace njBlockChain
+{
+    class Block
+    {
+        public long Index { set; get; }
+
+        public DateTime timestamp { set; get; }
+
+        public Trx [] trxes { set; get; }
+
+        public int proof { set; get; }
+
+        public string previous_hash { set; get; }
+
+        public override string ToString()
+        {
+            return string.Format("{0}{1}{2}{3}{4}" ,this.Index,
+                this.timestamp,
+                string.Join(';', this.trxes.Select(x=>x.ToString())),
+                this.proof,this.previous_hash);
+        }
+    }
+}
---
+++ njBlockChain/Trx.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace njBlockChain
+{
+    class Trx
+    {
+        public string sender { set; get; }
+        public string recipient { set; get; }
+        public decimal amount { set; get; }
+    }
+}
