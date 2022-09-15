1b5a8a5840e15b73776f390154ee0bfb96c36cb1
njnj <njnj@phost.nj>
2022-09-15 19:26:53 +0430

    create block chain project

---
+++ njBlockChain/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace njBlockChain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
